cf7f22b529c3d161fe59ed805310fa7ce096baa6
Teala-B <bradleyteala@gmail.com>
2023-10-18 12:26:38 -0500

    changed tip to fixed 2

--- tips.js
+++ tips.js
@@ -8,5 +8,5 @@ var totalDue = foodCost + tax + tip;
 
 //output
 console.log("Food cost is " + foodCost + " and tax is " + tax);
-console.log("Tip is " + tip);
+console.log("Tip is " + tip.toFixed(2));
 console.log("Total due is " + totalDue);
