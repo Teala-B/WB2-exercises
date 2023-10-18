322e9a248c946ffc8a0664d238bca9f582db306f
Teala-B <bradleyteala@gmail.com>
2023-10-18 16:57:07 -0500

    added script to taxes file

---
+++ MathScripts/taxes.js
@@ -0,0 +1,7 @@
+var salary = 1000
+var taxes = .23
+var taxesPercent = 23
+
+var moneyHeld = salary * taxes
+
+console.log("A salary of $" + salary + " with federal taxes of " + taxesPercent + "% has $" + moneyHeld + " held")
