94c763d503d36b5cc2f1256674b17dff66d52a48
Teala-B <bradleyteala@gmail.com>
2023-10-18 15:22:59 -0500

    added script to area of circle page

---
+++ MathScripts/area_of_cicle.js
@@ -0,0 +1,6 @@
+var radius = 3;
+Math.PI;
+
+var circleArea = Math.PI * radius * radius
+
+console.log("The area of a cricle with a radius of " + radius + " is " + circleArea.toFixed(2))
--- MathScripts/tip_amount.js
+++ MathScripts/tip_amount.js
@@ -1,6 +1,6 @@
-var foodBill = 35
-var tipPercent = .18
+var foodBill = 35;
+var tipPercent = .18;
 
-var tipAmount = foodBill * tipPercent
+var tipAmount = foodBill * tipPercent;
 
-console.log("The tip on a $" + foodBill +" food bill is $" + tipAmount.toFixed(2))
+console.log("The tip on a $" + foodBill +" food bill is $" + tipAmount.toFixed(2));
