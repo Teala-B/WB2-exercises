cab09848d479c9e38b1774b469b9a95eb6da0a9e
Teala-B <bradleyteala@gmail.com>
2023-10-19 18:57:54 -0500

    added script to show majors

--- IfScripts/show_major.js
+++ IfScripts/show_major.js
@@ -1,2 +1,34 @@
-let studentName = Betty
-let majorCode = ENG
+
+let majorCode = "HIST";
+
+switch(majorCode)
+{
+    case "BIOL":
+        console.log("Student: Terra");
+        console.log("Major: Biology");
+        console.log("Advising Location: Science Bldg, Room 310");
+        break;
+    case "CSCI":
+        console.log("Student: Sammi");
+        console.log("Major: Coumputer Science");
+        console.log("Advising Location: Sheppard Hall, Room 314");
+        break;
+    case "ENG":
+        console.log("Student: Lexi");
+        console.log("Major: English");
+        console.log("Advising Location: Kerr Hall, Room 201");
+        break;
+    case "HIST":
+        console.log("Student: Alex");
+        console.log("Major: History");
+        console.log("Advising Location: Kerr Hall, Room 114");
+        break;
+    case "MKT":
+        console.log("Student: Naya");
+        console.log("Major: Marketing");
+        console.log("Advising Location: Westly Hall, Room 310");
+        break;
+    default:
+        console.log("Not a department");
+        break;
+}
