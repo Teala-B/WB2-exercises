adfb412138b4ee1ab36e825bf2aae9f599376c42
Teala-B <bradleyteala@gmail.com>
2023-10-19 15:58:28 -0500

    made script fo min and max activity file

---
+++ IfScripts/min_max.js
@@ -0,0 +1,24 @@
+let a = 2
+let b = 5
+let c = 8
+
+if (a>b && a>c) {
+   console.log("The largest number is " + a);
+}
+
+else if (b>a && b>c) {
+    console.log("The largest number is " + b);
+}
+else {
+    console.log("The largest number is " + c);
+}
+if (a<b && a<c) {
+    console.log("The smallest number is " + a);
+ }
+ 
+ else if (b<a && b<c) {
+     console.log("The smallest number is " + b);
+ }
+ else {
+     console.log("The smallest number is " + c);
+ }
